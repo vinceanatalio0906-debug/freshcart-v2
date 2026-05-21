e7ca8f59ca9b21d82628da11fe45bac1c761511f
vinceanatolio0906-debug <vinceanatalio0906@gmail.com>
2026-05-21 14:34:41 +0800

    update routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,1 @@
-const express = require('express');
-const router = express.Router();
-
-// Halimbawa para sa orderRoutes.js
-router.get('/', async (req, res) => {
-    try {
-        // Dito mo ilalagay ang logic para makuha ang orders sa database
-        res.status(200).json({ message: "Order route is working!" });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-module.exports = router;
+module.exports = require("../../routes/productRoutes");
